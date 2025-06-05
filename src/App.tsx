9deff524558e5af5e73d6d58abfb6e39629c1d17
JulianaGarciaCo <julianageraldineco@gmail.com>
2025-06-05 14:38:44 -0500

    Create CookingSection layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CookingSection from "./features/cookingSection/CookingSection"
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="w-full px-4 py-8 md:px-12 md:py-12 lg:px-[150px] lg:py-[150px] mx-auto">
+      <CookingSection />
+    </main>
   )
 }
 
